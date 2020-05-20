7147053c99c7aa7903f4a6287ec8de17bd6cf09c
Smailsa97 <sahil.smail97@gmail.com>
2020-05-20 12:52:24 +0200

    ajout de la liste 2

---
+++ app/src/main/java/com/example/racel/Pokemon.java
@@ -0,0 +1,18 @@
+package com.example.racel;
+
+public class Pokemon {
+
+    private String name;
+    private String url;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+
+}
+
